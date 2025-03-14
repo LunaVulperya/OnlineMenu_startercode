9e81c8456d7372de9fcb71d876d2674619f0e2e0
LunaVulperya <lunavulperya@gmail.com>
2025-03-14 20:29:53 -0300

    Added JavaScript Coding

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Simulação de adição de produtos a cesta do site
+const addbutton = document.querySelectorAll(".additem");
+const listaPedido = document.getElementById("lista-pedido");
+const totalElemento = document.getElementById("total");
+let total = 0;
+
